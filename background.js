76065f2acf881d97b86438e7dba9a6600462efc3
NickGreenSF <nicholasgreensf@gmail.com>
2021-04-23 23:14:12 -0700

    checkboxes

--- background.js
+++ background.js
@@ -1,8 +1,3 @@
 
 chrome.storage.sync.set({values: [["Bicycle", "11:30", "MTWUFSN", true], ["Have sex", "21:00", "MTWUFSN", false]]});
 
-setInterval(function(){
-  chrome.storage.sync.get(['values'], function(result) {
-    console.log('Value currently is ' + result.values[0]);
-  });
-}, 60*1000);
--- popup.js
+++ popup.js
@@ -1,7 +1,7 @@
 // POPUP.JS
 // Check each of the user's tasks to see if they're done for the day or not.
 
-let taskHolder = document.getElementById("taskDiv");
+let taskHolder = document.getElementById("taskTable");
 let values = undefined;
 try {
     chrome.storage.sync.get(['values'], function(result){
@@ -15,11 +15,41 @@ try {
     taskHolder.innerText = "There was a problem in retrieving the tasks.";
 }
 
+setInterval(function(){
+    chrome.storage.sync.get(['values'], function(result) {
+      console.log('Value currently is ' + result.values[0]);
+    });
+  }, 60*1000);
+
 function insertValues(taskHolder, values){
     console.log(values.length);
     let i;
     taskHolder.innerHTML = ``;
+    // This function uses two separate for loops to initalize the checkboxes and to add listeners to them.
+    // Doing both in one for loop produced a problem where the first checkbox would not receive its click listener.
     for (i = 0; i<values.length; i++){
-        taskHolder.innerHTML = taskHolder.innerHTML + `<div>${values[i]}</div>`;
+        const isTrue = values[i][3];
+        taskHolder.innerHTML = taskHolder.innerHTML + 
+        `<tr>
+            <td>
+                <input type="checkbox" id="checkbox${i}" ${isTrue ? `checked` : ``}>
+            </td>
+            <td>
+                ${values[i][0]}
+            </td>
+            <td>
+                ${values[i][1]}
+            </td>
+        </tr>`;
     }
+    for (i = 0; i<values.length; i++){
+        console.log("checkbox"+i);
+        document.getElementById("checkbox"+i).addEventListener("click", function(){
+            checkBoxClick(this.id);
+        });
+    }
+}
+
+function checkBoxClick(id){
+    console.log(id);
 }
